0d6beec38bc173bd4e88ddd43b2599d4fec03a2a
Leftyxiv <manny@aledoux.net>
2021-08-18 07:33:32 -0400

    added the sorter class

--- build/index.js
+++ build/index.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log('hello');
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    ;
+    Sorter.prototype.sort = function () {
+    };
+    return Sorter;
+}());
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('hello');
+class Sorter {
+  constructor(public collection: number[]) {};
+  sort(): void {
+    
+  }
+}
